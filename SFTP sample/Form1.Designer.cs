6ce307bde01a38a2080a4d8e33414239bcacbc35
Redentorito Rodriguez <reden.rodriguez@asianterminals.com.ph>
2023-07-18 13:49:32 +0800

    Add project files.

---
+++ SFTP sample/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SFTP_sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPpkPathFile = new System.Windows.Forms.TextBox();
+            this.txtPkPassphrase = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPpkPathFile
+            // 
+            this.txtPpkPathFile.Location = new System.Drawing.Point(125, 64);
+            this.txtPpkPathFile.Name = "txtPpkPathFile";
+            this.txtPpkPathFile.Size = new System.Drawing.Size(269, 20);
+            this.txtPpkPathFile.TabIndex = 1;
+            // 
+            // txtPkPassphrase
+            // 
+            this.txtPkPassphrase.Location = new System.Drawing.Point(125, 90);
+            this.txtPkPassphrase.Name = "txtPkPassphrase";
+            this.txtPkPassphrase.Size = new System.Drawing.Size(269, 20);
+            this.txtPkPassphrase.TabIndex = 2;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(125, 38);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(269, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(125, 12);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(269, 20);
+            this.txtHost.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "privateKeyFilePath";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "privateKeyPassphrase";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "File path";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(125, 116);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(269, 20);
+            this.txtFilePath.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 173);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPkPassphrase);
+            this.Controls.Add(this.txtPpkPathFile);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPpkPathFile;
+        private System.Windows.Forms.TextBox txtPkPassphrase;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFilePath;
+    }
+}
+
---
+++ SFTP sample/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using Renci.SshNet;
+using System.Windows.Forms;
+
+namespace SFTP_sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // SFTP server connection details
+                string host = txtHost.Text;
+                string username = txtUsername.Text;
+                string privateKeyFilePath = txtPpkPathFile.Text;
+                string privateKeyPassphrase = txtPkPassphrase.Text;
+                string localFilePath = txtFilePath.Text; // Replace with the path to the local file you want to send
+
+                PrivateKeyFile privateKeyFile = new PrivateKeyFile(privateKeyFilePath, privateKeyPassphrase);
+                PrivateKeyAuthenticationMethod privateKeyAuth = new PrivateKeyAuthenticationMethod(username, privateKeyFile);
+
+                // Connect to the SFTP server
+                ConnectionInfo connectionInfo = new ConnectionInfo(host, username, privateKeyAuth);
+                using (SftpClient sftpClient = new SftpClient(connectionInfo))
+                {
+                    sftpClient.Connect();
+                    if (sftpClient.IsConnected)
+                    {
+                        MessageBox.Show("Connected");
+                        // Upload the file to the SFTP server
+                        using (FileStream oFileStream = new FileStream(localFilePath, FileMode.Open))
+                        {
+                            string filename = Path.GetFileName(localFilePath);
+                            sftpClient.UploadFile(oFileStream, filename);
+                        }
+                        // Check if the file exists on the server
+                        bool fileExists = sftpClient.Exists(Path.GetFileName(localFilePath));
+                        if (fileExists)
+                        {
+                            MessageBox.Show("File uploaded successfully.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("File upload failed or file does not exist on the server.");
+                        }
+                        sftpClient.Disconnect();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not Connected");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
